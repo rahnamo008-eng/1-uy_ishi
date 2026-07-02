12a9444d2782af9b9734a6380a48b9fa57b8f376
unknown <muhammadyusufsevgi@gmail.com>
2026-07-02 14:29:13 +0500

    2-qadam: Arifmetik funksiyalar qo'shildi

--- functions.py
+++ functions.py
@@ -6,3 +6,18 @@ def kvadrat(a):
 
 def kub(a):
     return a ** 3
+
+
+def yigindi(a, b):
+    return a + b
+
+def ayirma(a, b):
+    return a - b
+
+def kopaytma(a, b):
+    return a * b
+
+def bolish(a, b):
+    if b != 0:
+        return a / b
+    return "Nolga bo'lish mumkin emas!"
